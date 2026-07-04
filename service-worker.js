c6b57f9a3cf3043ac86858fe2b03f22b43067f11
Vidyasagar <vidyasagarchouhan9@gmail.com>
2026-07-04 18:00:27 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "petri-chor-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./game.html",
+  "./favicon.png",
+  "./petri-chor-cover.jpg",
+  "./star.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
