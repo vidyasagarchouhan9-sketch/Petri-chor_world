69c786526b8ac0879b5ce6db69d997bc69b23e17
Vidyasagar <vidyasagarchouhan9@gmail.com>
2026-07-05 17:40:07 +0530

    Update input.js

--- game/input.js
+++ game/input.js
@@ -1,11 +1,9 @@
-//coming soon 
+const keys = {};
 
+window.addEventListener("keydown", (e) => {
+  keys[e.key.toLowerCase()] = true;
+});
 
-
-
-
-
-
-
-
-
+window.addEventListener("keyup", (e) => {
+  keys[e.key.toLowerCase()] = false;
+});
