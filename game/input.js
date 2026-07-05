db9456cc1f7ddd50bac05c9ee503183899fe26fb
Vidyasagar <vidyasagarchouhan9@gmail.com>
2026-07-05 19:32:01 +0530

    Update input.js

--- game/input.js
+++ game/input.js
@@ -4,40 +4,48 @@ const stick = document.getElementById("stick");
 let moveX = 0;
 let moveY = 0;
 
-joystick.addEventListener("touchmove", function(e){
+const rect0 = joystick.getBoundingClientRect();
+const center = rect0.width / 2;
+const max = center - stick.getBoundingClientRect().width / 2;
 
-    e.preventDefault();
-
+function updateStick(clientX, clientY) {
     const rect = joystick.getBoundingClientRect();
 
-    const touch = e.touches[0];
+    let x = clientX - rect.left - center;
+    let y = clientY - rect.top - center;
 
-    let x = touch.clientX - rect.left - 60;
-    let y = touch.clientY - rect.top - 60;
+    const dist = Math.sqrt(x * x + y * y);
 
-    const max = 35;
-
-    const dist = Math.sqrt(x*x + y*y);
-
-    if(dist > max){
-        x = x / dist * max;
-        y = y / dist * max;
+    if (dist > max) {
+        x = (x / dist) * max;
+        y = (y / dist) * max;
     }
 
-    stick.style.left = (35 + x) + "px";
-    stick.style.top = (35 + y) + "px";
+    stick.style.left = (center + x) + "px";
+    stick.style.top = (center + y) + "px";
 
     moveX = x / max;
     moveY = y / max;
+}
 
-});
-
-joystick.addEventListener("touchend", function(){
-
-    stick.style.left = "35px";
-    stick.style.top = "35px";
-
+function resetStick() {
+    stick.style.left = center + "px";
+    stick.style.top = center + "px";
     moveX = 0;
     moveY = 0;
+}
 
-});
+joystick.addEventListener("touchstart", function (e) {
+    e.preventDefault();
+    const touch = e.touches[0];
+    updateStick(touch.clientX, touch.clientY);
+}, { passive: false });
+
+joystick.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+    const touch = e.touches[0];
+    updateStick(touch.clientX, touch.clientY);
+}, { passive: false });
+
+joystick.addEventListener("touchend", resetStick);
+joystick.addEventListener("touchcancel", resetStick);
