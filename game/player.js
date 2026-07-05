95874c37edfa4e82a4f6e4d5ae456a301707e417
Vidyasagar <vidyasagarchouhan9@gmail.com>
2026-07-05 20:39:53 +0530

    Update player.js

--- game/player.js
+++ game/player.js
@@ -1,9 +1,15 @@
 const player = {
-  x: 3,
-  y: 3,
+  x: 1.5,
+  y: 1.5,
 
   angle: 0,
 
-  moveSpeed: 0.08,
-  turnSpeed: 0.05
+  fov: Math.PI / 3,
+
+  walkSpeed: 0.045,
+  runSpeed: 0.09,
+  moveSpeed: 0.045,
+  turnSpeed: 0.045,
+
+  running: false
 };
