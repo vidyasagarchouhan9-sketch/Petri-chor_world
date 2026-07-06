5ac46e275a19b106f9f739f9fded733262dd3fbd
Vidyasagar <vidyasagarchouhan9@gmail.com>
2026-07-06 20:19:50 +0530

    Add fetch logic to update notice badge

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("notice.html")
+.then(response => response.text())
+.then(html => {
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html,"text/html");
+
+    const total = doc.querySelectorAll(".notice").length;
+
+    const lastSeen = Number(localStorage.getItem("lastSeen")) || 0;
+
+    const badge = document.getElementById("badge");
+
+    if(total > lastSeen){
+        badge.textContent = total - lastSeen;
+        badge.style.display = "flex";
+    }
+
+});
